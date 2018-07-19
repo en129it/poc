b00a386ad1317f8fccfa79d24fbe47865bd209b1
en129it <dimdevr@gmail.com>
2018-07-20 00:27:39 +0200

    Update a.java

--- src/a.java
+++ src/a.java
@@ -79,3 +79,67 @@ Select Embedded Messaging
 		  topicPublisher.publish(topicSession.createTextMessage(“Hello world”));
 		} catch (JMSException e) {
 		}
+
+
+
+
+
+1.	The goal is to enforce the password policy currently implemented in TOTO.
+
+2.	The new policy applies only to any password typed in by users in the TOTO page. 
+
+Users can change their password at any time by selecting TOTO from the main menu or when they are forced to do so after having logged into TOTO when their current password has exceeded the maximum password usage age. That age is specified on a company basis and is TOTO days by default
+
+3.	As a consequence of 2., users whose current password does not meet the new policy rules will not be affected: they will be impacted only when they change their password.
+
+4.	The current password policy implemented in TOTO is:
+
+a.	Length must be between 8 and 32 characters
+b.	Inclusion of at least one upper case letter
+c.	Inclusion of at least one digit
+d.	No white space character
+e.	No reuse of a password in use during a specific retention period. That period is XXX months 
+
+5.	The current password policy will be enforced with the following rules: 
+
+a.	Inclusion of a special character. Special characters have ASCII code is in the range [33-126] (where the range bounds are decimal numbers) and are not in [0-9,A-Z,a-z]
+b.	Prohibition of words found in user personal information including the company to which there are attached.
+
+a.	Is considered as user personal information:
+b.	Is considered as company information:
+c.	Checking for the presence of words is case insensitive. This means that if ‘Bob’ is the first name of the connected user then for example ‘BOB’, ‘bOB’ and ‘bob’ are considered as similar to ‘Bob’
+d.	Each password part that is matching a. or b. in a case insensitive way is removed from the password. The resulting password after the removal is then tested against 4. and 5.a. to determine if the password meets the policy rules. For example if ‘Bob’ is the first name of the connected user then:
+Original password	Modified password	Is authorized?
+Toto1234*		Toto1234*		Yes  
+BOB1234*		1234*			No because does not meet 4.a.
+BOBAlice1234*		Alice1234*		Yes
+
+c.	Prohibition of passwords contained in a password black list.
+a.	The list is stored in a new TOTO database table in the TOTO database schema
+b.	The database table content can be modified at any time through SQL script
+c.	No TOTO page will provide the ability to read or manage that blacklist
+d.	Checking for the presence of a password in the blacklist is case insensitive. This means that if the blacklist contains ‘Password123’ then for example ‘PASSWORD123’, ‘passWord123’ and ‘password123’ are not authorized passwords. However ‘Password1234’ is authorized.
+
+6.	The TOTO page is modified as follow:
+
+a.	Option 1: when the user submits a new password that does not meet the policy rules 
+	4. and 5.a.-b. then “Your password does not meet the password policy rules. Click here to access the policy rules” is displayed is a red warning banner. The keyword “here” is in bold and is a hyperlink to the Password Policy page.
+	5.c. then “Your password is too common and can be easily guessed” is displayed is a red warning banner
+b.	Option 2: a “Password Policy Rules” hyperlink is displayed at the right of the Save and Cancel buttons. That link brings the user to the TOTO page. When the user submits a new password that does not meet the policy rules
+	4. and 5.a.-b. then “Your password does not meet the password policy rules” is displayed in a red banner.
+	5.c. then “Your password is too common and can be easily guessed” is displayed is a red warning banner
+
+7.	The TOTO page is a new page with following content:
+
+Password policy rules:
+•	Length
+Passwords must have at least 8 characters and cannot exceed 32 characters
+•	Complexity
+Passwords must contain the following four characters types: lower case letter, upper case letter, digits and symbols
+Passwords cannot contain white spaces
+•	History
+Passwords that were in use during the last XX months cannot be re-used
+•	User personal information
+Passwords may contain personal user information however that information is ignored when testing the password complexity strength. So if your first name is ‘Bob’ then password ‘BOB1234a@’ is not authorized because ignoring ‘BOB’ the password length is only 6 characters. Personal information includes:  
+
+Back button: redirects the user to the previous page
